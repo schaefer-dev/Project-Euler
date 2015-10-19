7314fdbab1a0a2f4a1c8c3cf7caa2a7fe2287df3
Daniel Schäfer <schaefer.dev@gmail.com>
2015-10-19 16:54:49 +0200

    problem12 started all over again, I need to use Sieve of Atkin

--- problem012/problem012.py
+++ problem012/problem012.py
@@ -1,37 +1,14 @@
-nextadd = 1
-triangle = 0
+triangle = 1
+number = 1
+nextadd = 2
 
-progress = 0
-stepstone = 0
+def divisorcheck(x):
 
-
-def extraTest(value):
-    if (value % 4 != 0):
-        return False
-    if (value % 3 != 0):
-        return False
-    return True
 
 
 while(True):
     triangle += nextadd
+    number += 1
     nextadd += 1
-    progress += 1
-    if (progress % 100 == 0):
-        stepstone += 1
-        print('step: ' + str(stepstone) + '00 with triangle: ' + str(triangle))
-
-    teiler = 2
-    teilercount = 1
-
-    while((teiler + (498 - teilercount) <= (triangle / 2)) and (teilercount != 500)):
-        if (not(extraTest(triangle))):
-            break
-        if (triangle % teiler == 0):
-            teilercount += 1
-        teiler += 1
-
-    if teilercount == 500 :
-        print(triangle)
-        exit
+    divisorcheck(triangle)
 
