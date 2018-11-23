2c5dfd5bb17b7299dccf53a7fd8588ff6ba931bd
Daniel Schäfer <schaefer.dev@gmail.com>
2018-11-23 14:17:16 +0100

    improved rust solution for problem002

--- problem002/src/main.rs
+++ problem002/src/main.rs
@@ -8,17 +8,12 @@ fn fibonacci_sequence_even_sum(limit: &u64) -> u64{
     let mut result:u64 = 0;
 
     let mut fib = Fibonacci {fib1: 1, fib2: 2};
-    let mut help:u64;
 
-    while fib.fib2 < *limit {
-        if fib.fib2 % 2 == 0 {
+    while fib.smaller_than(&limit) {
+        if fib.is_even() {
             result += fib.fib2;
         }
-
-        help = fib.fib2;
-        // calculate next fibunaci number
-        fib.fib2 = fib.fib2 + fib.fib1;
-        fib.fib1 = help;
+        fib.next();
     }
 
     result
@@ -29,3 +24,26 @@ struct Fibonacci {
     fib2: u64,
 }
 
+impl Fibonacci {
+
+    // calculate next fibonacci number
+    fn next(&mut self) -> () {
+        let help = self.fib2;
+        self.fib2 = self.fib2 + self.fib1;
+        self.fib1 = help;
+    }
+
+    fn smaller_than(&self, limit: &u64) -> bool {
+        if self.fib1 < *limit {
+            return true;
+        }
+        false
+    }
+
+    fn is_even(&self) -> bool {
+        if self.fib2 % 2 == 0 {
+            return true;
+        }
+        false
+    }
+}
